7fbffe555a5f621814ff248ad50ca6ac061ae4a7
Vando Diniz <vandocarlos2001@outlook.com>
2022-06-18 16:24:09 -0300

    modularizacao certa

--- uteis.cpp
+++ uteis.cpp
@@ -4,42 +4,47 @@
 #include <stdio.h>
 using namespace std;
 
-void incrementa_contador(int *contador){
+void incrementa_contador(int* contador) {
     /*
-    Função responsável por incrementar o valor de um determinado contador. O contador é resetado após passar de 999999. Função bastante importante para gerar os NSEQs.
-    
-    Args: 
+    Função responsável por incrementar o valor de um determinado contador. O contador é resetado após passar de 999999. 
+    Função bastante importante para gerar os NSEQs.
+
+    Args:
         contador (pointer): endereço de memória do contador.
     */
 
-    *contador+=1;
+    *contador += 1;
 
     if (*contador > 999999)
         *contador = 1;
 }
 
-string gera_NSEQ(int contador){ 
+string gera_NSEQ(int contador) {
     /*
-    Função responsável por gerar um NSEQ. Primeiramente, salva-se o valor do contador em uma nova variável do tipo string e calcula-se o tamanho desta. A partir daí, cria-se em um ‘for loop’ uma nova string auxiliar com a quantidade de zeros necessárias para que o NSEQ tenha seis dígitos. Feito isso, concatenamos essas strings e a retornamos.
-    
-    Args: 
+    Função responsável por gerar um NSEQ. Primeiramente, salva-se o valor do contador em uma nova variável do tipo string e 
+    calcula-se o tamanho desta. A partir daí, cria-se em um ‘for loop’ uma nova string auxiliar com a quantidade de zeros 
+    necessárias para que o NSEQ tenha seis dígitos. Feito isso, concatenamos essas strings e a retornamos.
+
+    Args:
         contador (int): valor bruto do NSEQ a ser gerado.
     Returns: string de 6 elementos.
-    */   
+    */
 
     string NSEQ;
     string contador_string = to_string(contador);
-    for(int i=0; i < 6-contador_string.length(); i++)
-        NSEQ+= '0';
-    NSEQ+=contador_string;
+    for (int i = 0; i < 6 - contador_string.length(); i++)
+        NSEQ += '0';
+    NSEQ += contador_string;
     return NSEQ;
 }
 
-string gera_N(int tamanho){
+string gera_N(int tamanho) {
     /*
-    Função responsável por gerar uma string composta por N dígitos aleatórios. Com esta função gera-se os valores dos parâmetros VOL, ID e PRIORIDADE. Nela cada dígito é sorteado e armazenado em strings. O número final é a concatenação dessas strings. 
-    
-    Args: 
+    Função responsável por gerar uma string composta por N dígitos aleatórios. 
+    Com esta função gera-se os valores dos parâmetros VOL, ID e PRIORIDADE. 
+    Nela cada dígito é sorteado e armazenado em strings. O número final é a concatenação dessas strings.
+
+    Args:
         tamanho (int): quantidade de digitos do número que será gerado
     Return: string de N elementos.
     */
@@ -49,64 +54,74 @@ string gera_N(int tamanho){
     string N;
 
     //Definição do caso improvável
-    for(int i=0; i<tamanho-1; i++)
+    for (int i = 0; i < tamanho - 1; i++)
         minimo += "0";
 
     //Sorteia os N números e compara com o caso improvável
-    for(int i=0; i<tamanho; i++){
-            if (N==minimo)
-                num = rand()%9 + 1;
-            else
-                num = rand()%10;
-            N += to_string(num);
-        }         
+    for (int i = 0; i < tamanho; i++) {
+        if (N == minimo)
+            num = rand() % 9 + 1;
+        else
+            num = rand() % 10;
+        N += to_string(num);
+    }
     return N;
 }
 
-string gera_NNNNpN(){
+string gera_NNNNpN() {
     /*
-    Função responsável por gerar uma string composta por 5 dígitos aleatórios e um ponto na quinta posição. Com esta função gera-se os valores dos parâmetros SP_PRESS, SP_TEMP, PRESSAO_T, TEMP, PRESSAO_G e  NÍVEL. Funcionamento similar a função anterior.
-    
+    Função responsável por gerar uma string composta por 5 dígitos aleatórios e um ponto na quinta posição. 
+    Com esta função gera-se os valores dos parâmetros SP_PRESS, SP_TEMP, PRESSAO_T, TEMP, PRESSAO_G e  NÍVEL. 
+    Funcionamento similar a função anterior.
+
     Args: -
     Returns: string de 6 elementos.
-    */  
+    */
 
     int num;
     string NNNNpN;
-    for(int i=0; i<6; i++){
-            if (i==4)
-                NNNNpN+=".";
-            else{
-                num = rand()%10;
-                NNNNpN += to_string(num);
-            }
+    for (int i = 0; i < 6; i++) {
+        if (i == 4)
+            NNNNpN += ".";
+        else {
+            num = rand() % 10;
+            NNNNpN += to_string(num);
         }
+    }
     return NNNNpN;
 }
 
-string gera_tempo(){
+string gera_tempo() {
     /*
-    Função responsável por capturar o horário do sistema e transformá-lo em uma string. Necessária para a criação do parâmetro TIMESTAMP.
+    Função responsável por capturar o horário do sistema e transformá-lo em uma string. Necessária para a criação do 
+    parâmetro TIMESTAMP.
     Args: None.
     Returns: string de 8 elementos.
-    */  
-    
+    */
+
+    SYSTEMTIME st = { 0 };
+
+    GetLocalTime(&st);
+
     string TIMESTAMP;
-    time_t result = time(nullptr);
-    string tempo = asctime(localtime(&result));
-    for (int i = 11; i<19; i++)
-        TIMESTAMP += tempo[i];
+    TIMESTAMP += to_string(st.wHour);
+    TIMESTAMP += ":";
+    TIMESTAMP += to_string(st.wMinute);
+    TIMESTAMP += ":";
+    TIMESTAMP += to_string(st.wSecond % 02);
     return TIMESTAMP;
 }
 
-string gera_mensagem_otimizacao(int* contador){
+string gera_mensagem_otimizacao(int* contador) {
     /*
-    Função responsável por gerar uma mensagem de otimização. Primeiramente, incrementamos o contador e a partir daí geramos os valores de cada parâmetro individualmente com ajuda das funções anteriores. Feito isso, concatenamos todas essas strings junto com o caractere "|" para representar a mensagem final.
+    Função responsável por gerar uma mensagem de otimização. Primeiramente, incrementamos o contador e a partir daí geramos 
+    os valores de cada parâmetro individualmente com ajuda das funções anteriores. Feito isso, concatenamos todas essas strings 
+    junto com o caractere "|" para representar a mensagem final.
 
-    Args: 
+    Args:
         contador (int): valor bruto do NSEQ a ser gerado.
     Returns: string de 38 caracteres.
-    */  
+    */
 
     incrementa_contador(contador);
 
@@ -116,18 +131,20 @@ string gera_mensagem_otimizacao(int* contador){
     string SP_TEMP = gera_NNNNpN();
     string VOL = gera_N(5);
     string TIMESTAMP = gera_tempo();
-    
+
     return NSEQ + "|" + TIPO + "|" + SP_PRESS + "|" + SP_TEMP + "|" + VOL + "|" + TIMESTAMP;
 }
 
-string gera_mensagem_scada_processo(int* contador){
+string gera_mensagem_scada_processo(int* contador) {
     /*
-    Função responsável por gerar uma mensagem do SCADA referentes aos dados do processo. Primeiramente, incrementamos o contador e a partir daí geramos os valores de cada parâmetro individualmente com ajuda das funções anteriores. Feito isso, concatenamos todas essas strings junto com o caractere "|" para representar a mensagem final.
+    Função responsável por gerar uma mensagem do SCADA referentes aos dados do processo. Primeiramente, incrementamos 
+    o contador e a partir daí geramos os valores de cada parâmetro individualmente com ajuda das funções anteriores. 
+    Feito isso, concatenamos todas essas strings junto com o caractere "|" para representar a mensagem final.
 
-    Args: 
+    Args:
         contador (int): valor bruto do NSEQ a ser gerado.
     Returns: string de 46 caracteres.
-    */  
+    */
 
     incrementa_contador(contador);
 
@@ -138,19 +155,21 @@ string gera_mensagem_scada_processo(int* contador){
     string PRESSAO_G = gera_NNNNpN();
     string NIVEL = gera_NNNNpN();
     string TIMESTAMP = gera_tempo();
-    
+
     return NSEQ + "|" + TIPO + "|" + PRESSAO_T + "|" + TEMP + "|" + PRESSAO_G + "|" + NIVEL + "|" + TIMESTAMP;
 }
 
-string gera_mensagem_scada_alarmes(int* contador){
+string gera_mensagem_scada_alarmes(int* contador) {
     /*
-    Função responsável por gerar uma mensagem do SCADA referentes aos alarmes. Primeiramente, incrementamos o contador e a partir daí geramos os valores de cada parâmetro individualmente com ajuda das funções anteriores. Feito isso, concatenamos todas essas strings junto com o caractere "|" para representar a mensagem final.
+    Função responsável por gerar uma mensagem do SCADA referentes aos alarmes. Primeiramente, incrementamos o 
+    contador e a partir daí geramos os valores de cada parâmetro individualmente com ajuda das funções anteriores. 
+    Feito isso, concatenamos todas essas strings junto com o caractere "|" para representar a mensagem final.
 
-    Args: 
+    Args:
         contador (int): valor bruto do NSEQ a ser gerado.
     Returns: string de 27 caracteres.
-    */  
-   
+    */
+
     incrementa_contador(contador);
 
     string NSEQ = gera_NSEQ(*contador);
@@ -158,6 +177,6 @@ string gera_mensagem_scada_alarmes(int* contador){
     string ID = gera_N(4);
     string PRIORIDADE = gera_N(3);
     string TIMESTAMP = gera_tempo();
-    
+
     return NSEQ + "|" + TIPO + "|" + ID + "|" + PRIORIDADE + "|" + TIMESTAMP;
 }
